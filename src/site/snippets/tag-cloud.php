950730c6c5eb0f646aae4736ed534b12cf81c180
James Drever <james@careful.digital>
2022-12-26 15:38:27 +0000

    Git ignore fix for kirby folder

--- src/site/snippets/tag-cloud.php
+++ src/site/snippets/tag-cloud.php
@@ -6,9 +6,9 @@ $tags = $page->children()->listed()->pluck('tags', ',', true);
 ?>
 <p>
   <strong>Tags: </strong>
-  <a href="<?= url('blog') ?>">All</a> |
+  <a href="<?= url('/commons/blog') ?>">All</a> |
   <?php foreach($tags as $tag): ?>
-  <a href="<?= url('blog', ['params' => ['tag' => $tag]]) ?>">
+  <a href="<?= url('/commons/blog', ['params' => ['tag' => $tag]]) ?>">
     <?= html($tag) ?>
   </a> |
   <?php endforeach ?>
