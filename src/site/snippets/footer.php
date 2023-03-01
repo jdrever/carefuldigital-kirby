99d05ac42793517a48859dead4b2e834b5dca3d8
James Drever <james@careful.digital>
2023-03-01 12:34:13 +0000

    adding width/height to cc logo

--- src/site/snippets/footer.php
+++ src/site/snippets/footer.php
@@ -8,7 +8,7 @@
 
 <footer>
   <p><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons Licence"
-        style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />The contents of
+        style="border-width:0" width="88" height="31" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />The contents of
     this Digital Commons are licensed under a <a rel="license"
       href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0
       International License</a>.</p>
