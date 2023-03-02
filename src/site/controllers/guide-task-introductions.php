0605c17257369d57220fb8be2ef65425da7bfb52
James Drever <james@careful.digital>
2023-03-02 12:04:30 +0000

    email sent if introduction added

--- src/site/controllers/guide-task-introductions.php
+++ src/site/controllers/guide-task-introductions.php
@@ -26,6 +26,29 @@ return function ($kirby, $page)
       'template' => 'guide-task-introduction',
     ]);
 
+    try {
+      $kirby->email([
+          'template' => 'email',
+          'from'     => 'james@careful.digital',
+          'replyTo'  => 'james@careful.digital',
+          'to'       => 'james@careful.digital',
+          'subject'  => 'You have an Introduction to check from a guide',
+          'data'     => [
+              'text' => 'Intentions:' . $intentions,
+          ]
+      ]);
+    }
+    catch (Exception $error) 
+    {
+      //TODO: proper handling of this error
+      if(option('debug')):
+          $alert['error'] = 'The form could not be sent: <strong>' . $error->getMessage() . '</strong>';
+      else:
+          $alert['error'] = 'The form could not be sent!';
+      endif;
+    }
+
+
     $allIntentions=$page->children();
 
     $taskComplete=true;
