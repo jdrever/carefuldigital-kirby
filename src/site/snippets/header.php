c82afb09a7f0709ed2c9f7394a8bb4a8c920aaa9
James Drever <james@careful.digital>
2023-03-02 11:42:20 +0000

    improved meta tags

--- src/site/snippets/header.php
+++ src/site/snippets/header.php
@@ -3,10 +3,10 @@
 
 <head>
   <meta charset="utf-8">
-  <title>The Careful Digital Commons <?=$page->title() ?></title>
+  <title>The Careful Digital Website and Commons: <?=$page->title() ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Guides by Careful Digital">
-  <meta name="author" content="James Drever">
+  <meta name="description" content="A website by the Careful Digital which provides resources about using technology intentionally">
+  <meta name="author" content="James Drever, Careful Digital">
   <!-- Fathom - beautiful, simple website analytics -->
   <script src="https://carefuld-fathom-usage-stats.careful.digital/script.js" data-site="PUGDMEZB" defer></script>
   <!-- / Fathom -->
