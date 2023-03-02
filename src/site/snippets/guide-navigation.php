b21847593a780c5493badf337763ed76a3bb403a
James Drever <james@careful.digital>
2023-03-02 11:43:41 +0000

    adding colon to NEXT/PREVIOUS buttons

--- src/site/snippets/guide-navigation.php
+++ src/site/snippets/guide-navigation.php
@@ -1,7 +1,7 @@
 <?php if ($page->hasPrev()): ?>
-  <a href="<?= $page->prev()->url() ?>">← PREVIOUS <?=$page->prev()->title()?></a>
+  <a href="<?= $page->prev()->url() ?>">← PREVIOUS: <?=$page->prev()->title()?></a>
 <?php else: ?>
-  <a href="<?= $page->parent()->url() ?>">← PREVIOUS <?=$page->parent()->title()?></a>
+  <a href="<?= $page->parent()->url() ?>">← PREVIOUS: <?=$page->parent()->title()?></a>
 <?php endif ?>
 
 
@@ -10,11 +10,11 @@
 <?php else : ?>
   <?php if ($page->hasChildren()): ?>
     <a class="btn btn-primary m-2" href="<?= $page->children()->first()->url() ?>" role="button">
-          NEXT  <?=$page->children()->first()->title()?> →
+          NEXT:  <?=$page->children()->first()->title()?> →
     </a>
   <?php elseif ($page->hasNext()): ?>
     <a class="btn btn-primary m-2" href="<?= $page->next()->url() ?>" role="button">
-          NEXT <?= $page->next()->title() ?>  →
+          NEXT: <?= $page->next()->title() ?>  →
     </a>
   <?php endif ?>
 <?php endif ?>
