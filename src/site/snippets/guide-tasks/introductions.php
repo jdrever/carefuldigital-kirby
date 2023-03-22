0a47e67bf16c49010465af0f6d41b385694ad42e
James Drever <james@careful.digital>
2023-03-22 09:27:20 +0000

    changes to layout on intentions page

--- src/site/snippets/guide-tasks/introductions.php
+++ src/site/snippets/guide-tasks/introductions.php
@@ -1,6 +1,6 @@
 <form method="post">
   <label for="intentions" class="form-label">What are your intentions in using this guide?</label>
-  <textarea id="intentions" name="intentions" class="form-item"></textarea>
+  <textarea id="intentions" name="intentions" class="form-item" rows="10"></textarea>
   <?php snippet('honeypot')?>
   <?php snippet('guide-navigation', ['taskButton'=> 'SHARE YOUR INTENTION']) ?>
 </form>
