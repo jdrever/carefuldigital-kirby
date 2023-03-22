b3522cad7c83823e8fc38b354bc207ae2bea1fbc
James Drever <james@careful.digital>
2023-03-22 11:59:46 +0000

    retitling share your intentions button

--- src/site/snippets/guide-tasks/introductions.php
+++ src/site/snippets/guide-tasks/introductions.php
@@ -2,5 +2,5 @@
   <label for="intentions" class="form-label">What are your intentions in using this guide?</label>
   <textarea id="intentions" name="intentions" class="form-item" rows="10"></textarea>
   <?php snippet('honeypot')?>
-  <?php snippet('guide-navigation', ['taskButton'=> 'SHARE YOUR INTENTION']) ?>
+  <?php snippet('guide-navigation', ['taskButton'=> 'SHARE YOUR INTENTIONS']) ?>
 </form>
