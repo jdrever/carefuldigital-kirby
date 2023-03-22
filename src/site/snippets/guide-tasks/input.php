c6b1802af7aaea8290d47cb39f45ff5fdee46611
James Drever <james@careful.digital>
2023-03-22 17:15:49 +0000

    fixing textarea name on input form

--- src/site/snippets/guide-tasks/input.php
+++ src/site/snippets/guide-tasks/input.php
@@ -2,7 +2,7 @@
 <?php if ($page->labelText()->isNotEmpty()) : ?> 
   <label for="intentions" class="form-label"><?=$page->labelText()?></label>
 <?php endif ?>
-  <textarea id="intentions" name="intentions" class="form-item" rows="10"></textarea>
+  <textarea id="input" name="input" class="form-item" rows="10"></textarea>
   <?php snippet('honeypot')?>
   <?php snippet('guide-navigation', ['taskButton'=> $page->buttonText()->isNotEmpty() ? $page->buttonText() : $page->title()]) ?>
 </form>
