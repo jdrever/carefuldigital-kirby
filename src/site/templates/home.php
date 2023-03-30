20d157296a6845ebfe488f5a9b31f7a19a112863
James Drever <james@careful.digital>
2023-03-30 11:10:09 +0100

    home page layout changes

--- src/site/templates/home.php
+++ src/site/templates/home.php
@@ -11,8 +11,8 @@
       <p>
         Providing bespoke web development based on twenty five years experience
       </p>
+      <p class="mt-02"><a class="card-button" href="/development/">Find out more &rarr;</a></p>
     </div>
-    <a class="card-button" href="/development/">Find out more</a>
   </div>
   <div class="card-container">
     <div class="card-content">
@@ -20,8 +20,8 @@
       <p>
         Delivering effective digital projects that make people happy
       </p>
+      <p class="mt-02"><a class="card-button" href="/project-management/">Find out more &rarr;</a></p>
     </div>
-    <a class="card-button" href="/project-management/">Find out more</a>
   </div>
   <div class="card-container">
     <div class="card-content">
@@ -29,8 +29,9 @@
       <p>
         Creating a digital strategy for your business or charity, or helping to change your processes
       </p>
+      <p class="mt-02"><a class="card-button" href="/strategy/">Find out more &rarr;</a></p>
     </div>
-    <a class="card-button" href="/strategy/">Find out more</a>
+    
   </div>
   <div class="card-container">
     <div class="card-content">
@@ -38,8 +39,8 @@
       <p>
         Drawing on years of experience to help develop teams and individuals
       </p>
+      <p class="mt-02"><a class="card-button" href="/mentoring/">Find out more &rarr;</a></p>
     </div>
-    <a class="card-button" href="/mentoring/">Find out more</a>
   </div>
 </div>
 
@@ -48,31 +49,34 @@
 <div class="card-group-sm">
   <div class="card-container">
     <div class="card-content">
-      <h3><img src="/assets/images/people_logo.svg" alt="" width="60" height="60">People</h3>
+      <img src="/assets/images/people_logo.svg" alt="" width="60" height="60">
+      <h3>People</h3>
       <p>
         We focus first on making things better for people – we don’t start with the technology, that comes later
       </p>
+      <p class="mt-02"><a class="card-button" href="/people/">Find out more</a></p>
     </div>
-    <a class="card-button" href="/people/">Find out more</a>
   </div>
   <div class="card-container">
     <div class="card-content">
-      <h3><img src="/assets/images/craft_logo.svg" alt="" width="60" height="60">Craft</h3>
+    <img src="/assets/images/craft_logo.svg" alt="" width="60" height="60">
+      <h3>Craft</h3>
       <p>
         We make digital things that are built to evolve and flourish – we don’t rush towards a short-term fix
       </p>
     </div>
-    <a class="card-button" href="/craft/">Find out more</a>
+    <p class="mt-02"><a class="card-button" href="/craft/">Find out more</a></p>
   </div>
   <div class="card-container">
     <div class="card-content">
-      <h3><img src="/assets/images/consideration_logo.svg" alt="" width="60" height="60">Care</h3>
+      <img src="/assets/images/consideration_logo.svg" alt="" width="60" height="60">
+      <h3>Care</h3>
       <p>
         We think about the impact of the work we do on the wider environment and society – we don’t work
         in a vacuum
       </p>
+      <p class="mt-02"><a class="card-button" href="/consideration/">Find out more</a></p>
     </div>
-    <a class="card-button" href="/consideration/">Find out more</a>
   </div>
 </div>
 
