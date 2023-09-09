9838762ee1f723cc9e3ffb7bcf613eb06217742d
James Drever <james@careful.digital>
2023-09-09 16:46:14 +0100

    adding debug to contact form

--- src/site/templates/contact.php
+++ src/site/templates/contact.php
@@ -47,10 +47,10 @@
     <textarea id="text" name="text" class="form-item"  required><?= esc($data['text'] ?? '') ?></textarea>
     <?= isset($alert['text']) ? '<span class="alert error">' . esc($alert['text']) . '</span>' : '' ?>
   </div>
-  <?= $turnstileDiv ?>
+  *1<?= $turnstileDiv ?>
   <input type="submit" name="submit" value="Submit" class="btn">
 </form>
 <?php endif ?>
-<?= $turnstileJs ?>
+*2<?= $turnstileJs ?>
 
 <?php snippet('footer') ?>
